0ad547267ddb0f58416197739d682e2649ebcc27
unknown <enrique.arroyo@gmail.com>
2017-10-29 09:59:18 +0100

    On branch spark-openjdk  Changes to be committed: 	modified:   Spark-over-Docker/Dockerfile 	modified:   Spark-over-Docker/docker-compose.yml 	new file:   Spark-over-Docker/spark-submit.sh

---
+++ Spark-over-Docker/spark-submit.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+curl -X POST http://localhost:6066/v1/submissions/create \
+--header "Content-Type:application/json;charset=UTF-8" --data '{
+  "action" : "CreateSubmissionRequest",
+  "appArgs" : [ "/usr/local/spark-2.1.0-bin-hadoop2.7/data/mllib/kmeans_data.txt" ],
+  "appResource" : "file:/usr/local/spark-2.2.0-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.2.0.jar",
+  "clientSparkVersion" : "2.2.0",
+  "environmentVariables" : {
+    "SPARK_ENV_LOADED" : "1"
+  },
+  "mainClass" : "org.apache.spark.examples.mllib.KMeansExample",
+  "sparkProperties" : {
+    "spark.jars" : "file:/usr/local/spark-2.2.0-bin-hadoop2.7/examples/jars/*",
+    "spark.driver.supervise" : "false",
+    "spark.app.name" : "MyJob",
+    "spark.eventLog.enabled": "true",
+    "spark.submit.deployMode" : "cluster",
+    "spark.master" : "spark://localhost:6066"
+  }
+}'
