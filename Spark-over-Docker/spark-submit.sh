8038e9bcaaf54da600ad66f79775d8596dfd0bca
Enrique Arroyo <enrique.arroyo@gmail.com>
2017-12-27 12:18:00 +0100

    Update spark-submit.sh

--- Spark-over-Docker/spark-submit.sh
+++ Spark-over-Docker/spark-submit.sh
@@ -2,15 +2,15 @@
 curl -X POST http://spark-master:6066/v1/submissions/create \
 --header "Content-Type:application/json;charset=UTF-8" --data '{
   "action" : "CreateSubmissionRequest",
-  "appArgs" : [ "/usr/local/spark-2.2.0-bin-hadoop2.7/data/mllib/kmeans_data.txt" ],
-  "appResource" : "file:/usr/local/spark-2.2.0-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.2.0.jar",
-  "clientSparkVersion" : "2.2.0",
+  "appArgs" : [ "/usr/local/spark-2.2.1-bin-hadoop2.7/data/mllib/kmeans_data.txt" ],
+  "appResource" : "file:/usr/local/spark-2.2.1-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.2.1.jar",
+  "clientSparkVersion" : "2.2.1",
   "environmentVariables" : {
     "SPARK_ENV_LOADED" : "1"
   },
   "mainClass" : "org.apache.spark.examples.mllib.KMeansExample",
   "sparkProperties" : {
-    "spark.jars" : "file:/usr/local/spark-2.2.0-bin-hadoop2.7/examples/jars/*",
+    "spark.jars" : "file:/usr/local/spark-2.2.1-bin-hadoop2.7/examples/jars/*",
     "spark.driver.supervise" : "false",
     "spark.app.name" : "MyJob",
     "spark.eventLog.enabled": "true",
