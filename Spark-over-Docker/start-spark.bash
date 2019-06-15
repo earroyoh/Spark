87651be51093768582651ad3b788a7439334cb4c
earroyoh <enrique.arroyo@gmail.com>
2019-06-15 14:01:01 +0200

    modified:   Spark-over-Docker/Dockerfile 	modified:   Spark-over-Docker/start-spark.bash

--- Spark-over-Docker/start-spark.bash
+++ Spark-over-Docker/start-spark.bash
@@ -2,9 +2,9 @@
 
 #IFACE=enp0s3
 
-#curl -L https://github.com/kubernetes/kompose/releases/download/v1.17.0/kompose-linux-amd64 -o kompose
-#chmod +x kompose
-#sudo mv kompose /usr/local/bin
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.17.0/kompose-linux-amd64 -o kompose
+chmod +x kompose
+sudo mv kompose /usr/local/bin
 
 kompose convert -f docker-compose.yml
 kubectl create namespace spark
